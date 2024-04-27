78ce386017b9b611539aa4199645e56d85f72f63
Ashish kumar <ashishkumar0842a@gmail.com>
2024-04-28 00:20:34 +0530

    add first commit

---
+++ src/content_option.js
@@ -0,0 +1,227 @@
+const logotext = "aK";
+const meta = {
+    title: "Ashish Kumar",
+    description: "I’m Ashish Kumar data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Ashish Kumar",
+    animated: {
+        first: "Welcome to my portfolio",
+        second: "I am FullStack Developer",
+        third: "I Know Web and ML",
+    },
+    description: "Fullstack Web Developer fluent in Node.js and React, driven by Machine Learning. Excels in dynamic web apps, embraces innovation, and welcomes challenges to foster continuous growth in software development",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Dynamic Fullstack Developer: Node.js, React, and Machine Learning Enthusiast",
+    aboutme: "Fullstack Web Developer proficient in Node.js and React with a passion for Machine Learning. Skilled in building dynamic web applications and leveraging cutting-edge technologies to deliver innovative solutions. Excited about tackling challenges and continuously expanding knowledge in software development.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Automated Intelligent Document Processing Tool",
+        where: "Your Organization",
+        date: "2023 - Present",
+        description: "Developed an intelligent document processing tool using Machine Learning, OCR (Tesseract and EasyOCR), GANs, and NLP. Implemented with Python and Flask, data stored in MongoDB.",
+    },
+    {
+        jobtitle: "Property Management System",
+        where: "Your Organization",
+        date: "2022 - 2023",
+        description: "Developed a dynamic Property Management System using Node.js, ejs, Multer, Nodemailer, and MongoDB. Designed for efficient user interaction and management.",
+    },
+    {
+        jobtitle: "Real-Time Chat Application",
+        where: "Your Organization",
+        date: "2021 - 2022",
+        description: "Built a real-time chat application using Socket.IO for instant communication between users, enhancing collaboration and engagement.",
+    },
+    {
+        jobtitle: "Farmer's Space Equipment Rental Portal",
+        where: "Your Organization",
+        date: "2020 - 2021",
+        description: "Developed an equipment rental portal for farmers using Node.js, EJS, and MongoDB, facilitating easy access to agricultural equipment.",
+    },
+    {
+        jobtitle: "Restaurant Management System",
+        where: "Your Organization",
+        date: "2019 - 2020",
+        description: "Designed and implemented a Restaurant Management System using Node.js, EJS, and MongoDB, optimizing restaurant operations and customer service.",
+    },
+];
+
+
+const skills = [
+    {
+        name: "Node.js",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "HTML",
+        value: 70,
+    },
+    {
+        name: "CSS",
+        value: 70,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Flask",
+        value: 70,
+    },
+    {
+        name: "MongoDB",
+        value: 80,
+    },
+    {
+        name: "Machine Learning",
+        value: 85,
+    },
+    {
+        name: "Docker",
+        value: 75,
+    },
+    {
+        name: "Git",
+        value: 85,
+    },
+];
+
+
+const services = [
+    {
+        title: "Fullstack Web Development",
+        description: "Crafting dynamic web applications using Node.js, React, JavaScript, HTML, and CSS to deliver efficient and user-friendly experiences.",
+    },
+    {
+        title: "Python Development",
+        description: "Utilizing Python for backend development, data processing, and machine learning projects, ensuring robust and scalable solutions.",
+    },
+    {
+        title: "Database Management",
+        description: "Implementing MongoDB for efficient data storage and retrieval, optimizing database performance to meet application requirements.",
+    },
+    {
+        title: "Machine Learning Solutions",
+        description: "Applying machine learning algorithms and techniques to develop intelligent applications and enhance decision-making processes.",
+    },
+    {
+        title: "Web Development with Flask",
+        description: "Building web applications using Flask, a lightweight Python web framework, for rapid development and flexibility.",
+    },
+    {
+        title: "Version Control with Git",
+        description: "Using Git for efficient version control, enabling collaborative development and seamless code management.",
+    },
+    {
+        title: "Containerization with Docker",
+        description: "Leveraging Docker to streamline application deployment, ensuring consistency and portability across different environments.",
+    },
+];
+
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ashishkumar0842a@gmail.com",
+    YOUR_FONE: "+91 9135xxx842",
+    description: "Fullstack Web Developer fluent in Node.js and React, driven by Machine Learning. Excels in dynamic web apps, embraces innovation, and welcomes challenges to foster continuous growth in software development.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_csqqi5m",
+    YOUR_TEMPLATE_ID: "template_qzzi9th",
+    YOUR_USER_ID: "WSuQ5g3Ig2YKuo65y",
+};
+
+const socialprofils = {
+    github: "https://github.com/ashish842a?tab=repositories",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/ashish-kumar-98482b1a9/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
